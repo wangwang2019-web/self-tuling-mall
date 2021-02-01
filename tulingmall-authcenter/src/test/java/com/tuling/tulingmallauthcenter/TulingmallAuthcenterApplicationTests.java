8fdd35cc75e12dae882f270c494882976632b587
wangwei <2250412996@qq.com>
2021-02-01 16:26:58 +0800

    增加认证中心

---
+++ tulingmall-authcenter/src/test/java/com/tuling/tulingmallauthcenter/TulingmallAuthcenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuling.tulingmallauthcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TulingmallAuthcenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
